7e742bbe499a09cf2564d6096a33d97972f7cb8e
Elena <ivanova123402@mail.ru>
2022-11-12 15:47:02 +0300

    zadaha8

---
+++ DZ4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 8:
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N.
+int i = 1;
+bool not = true;
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Четные число от 1 до " + num);
+while (i <= num)
+{
+    if (i % 2 != 1)
+    {
+        Console.WriteLine(i + " ");
+        not = false;
+    }
+    i++;
+}
+if (not)
+{
+    Console.WriteLine("Нет четных чисел!");
+}
